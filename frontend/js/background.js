59fb06da39dfbe04988f228f1c61140174b8406a
benchan777 <benchan777@gmail.com>
2021-10-14 13:22:17 -0700

    move auth flow to background.js

--- frontend/js/background.js
+++ frontend/js/background.js
@@ -1,50 +1,54 @@
 let tabUrl;
 
-chrome.runtime.onMessage.addListener( (message, sender) => {
+chrome.runtime.onMessage.addListener((message, sender) => {
   if (message.message == 'get music') {
     tabUrl = message.url;
     getMusic();
   };
+
+  if (message.message == 'spotify login') {
+    spotifyLogin();
+  };
 })
 
 async function getMusic() {
-	const accessTokenTime = await getLocalValue("timeStamp");
-	const timeDifference = await elapsedTime(accessTokenTime.timeStamp);
+  const accessTokenTime = await getLocalValue("timeStamp");
+  const timeDifference = await elapsedTime(accessTokenTime.timeStamp);
 
-	// Generate new tokens if 50 min has elapsed since generation of last access token
-	if (timeDifference > 3000) {
-		const refreshToken = await getLocalValue("refreshToken");
-		const newToken = await getNewTokens(refreshToken.refreshToken);
+  // Generate new tokens if 50 min has elapsed since generation of last access token
+  if (timeDifference > 3000) {
+    const refreshToken = await getLocalValue("refreshToken");
+    const newToken = await getNewTokens(refreshToken.refreshToken);
 
-		// Store refreshed access token in database
-		chrome.storage.sync.set(
-			{
-				refreshToken: newToken.refresh_token,
-				accessToken: newToken.access_token,
-				timeStamp: Date.now(),
-			},
-			() => {
-				console.log("new access token stored successfully");
-			}
-		);
-	}
+    // Store refreshed access token in database
+    chrome.storage.sync.set(
+      {
+        refreshToken: newToken.refresh_token,
+        accessToken: newToken.access_token,
+        timeStamp: Date.now(),
+      },
+      () => {
+        console.log("new access token stored successfully");
+      }
+    );
+  }
 
-	const accessToken = await getLocalValue("accessToken");
-	const data = { videoUrl: tabUrl, accessToken: accessToken.accessToken };
+  const accessToken = await getLocalValue("accessToken");
+  const data = { videoUrl: tabUrl, accessToken: accessToken.accessToken };
 
-	fetch(`http://localhost:3000/api/like_song`, {
-		method: "POST",
-		mode: "cors",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify(data),
-	})
-		.then((response) => {
-			return response.json();
-		})
-		.then((data) => {
-			// chrome.runtime.sendMessage(data, (res) => {
-			// 	console.log("Track data sent to extension.");
-			// });
+  fetch(`http://localhost:3000/api/like_song`, {
+    method: "POST",
+    mode: "cors",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      // chrome.runtime.sendMessage(data, (res) => {
+      // 	console.log("Track data sent to extension.");
+      // });
       if (data.error) {
         songNotFound();
       };
@@ -52,58 +56,58 @@ async function getMusic() {
       if (data.title) {
         songAdded(data);
       };
-		})
-		.catch((error) => {
-			console.log(error);
-		});
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 // Retrieve data stored in local storage. Find item by key
 async function getLocalValue(key) {
-	return new Promise((resolve, reject) => {
-		chrome.storage.sync.get(key, (data) => {
-			resolve(data);
-		});
-	});
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(key, (data) => {
+      resolve(data);
+    });
+  });
 }
 
 // Get time difference between current time and time the last accessToken was generated
 async function elapsedTime(tokenTimestamp) {
-	return new Promise((resolve, reject) => {
-		const curentTime = Date.now();
-		const timeDifference = (curentTime - tokenTimestamp) / 1000;
-		resolve(timeDifference);
-	});
+  return new Promise((resolve, reject) => {
+    const curentTime = Date.now();
+    const timeDifference = (curentTime - tokenTimestamp) / 1000;
+    resolve(timeDifference);
+  });
 }
 
 // Generate new accessToken & refreshToken when old accessToken has expired
 // Refresh tokens obtained using PKCE auth flow can be exchanged for an access token only
 // once, so a new refresh token must be generated and stored each time
 async function getNewTokens(refreshToken) {
-	return new Promise((resolve, reject) => {
-		const grant_type = "refresh_token";
-		const client_id = "cc9e2365a9c1461ea9a251d446f347d0";
+  return new Promise((resolve, reject) => {
+    const grant_type = "refresh_token";
+    const client_id = "cc9e2365a9c1461ea9a251d446f347d0";
 
-		const queryString =
-			"https://accounts.spotify.com/api/token" +
-			`?client_id=${client_id}` +
-			`&grant_type=${grant_type}` +
-			`&refresh_token=${refreshToken}`;
+    const queryString =
+      "https://accounts.spotify.com/api/token" +
+      `?client_id=${client_id}` +
+      `&grant_type=${grant_type}` +
+      `&refresh_token=${refreshToken}`;
 
-		fetch(queryString, {
-			method: "POST",
-			mode: "cors",
-			headers: { "Content-Type": "application/x-www-form-urlencoded" },
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				// console.log(data)
-				resolve(data);
-			})
-			.catch((error) => {
-				reject(error);
-			});
-	});
+    fetch(queryString, {
+      method: "POST",
+      mode: "cors",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data)
+        resolve(data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
 
 // When song is found and added successfully to Spotify, the data is stored in local storage.
@@ -126,15 +130,15 @@ function songAdded(data) {
   })
 
   // Show notification badge
-  chrome.action.setBadgeBackgroundColor({ color: 'red'});
-  chrome.action.setBadgeText({ text: '1'});
+  chrome.action.setBadgeBackgroundColor({ color: 'red' });
+  chrome.action.setBadgeText({ text: '1' });
 };
 
 // Delete song info from local storage if song is not successfully added to Spotify. This
 // will trigger the chrome popup to notify the user that the song was not found and therefore
 // unable to be added.
 function songNotFound() {
-  chrome.storage.sync.remove(['addedSongTitle', 'addedSongArtist', 'songAddedTime' ], () => {
+  chrome.storage.sync.remove(['addedSongTitle', 'addedSongArtist', 'songAddedTime'], () => {
     chrome.storage.sync.set({ error: 'Song not found' })
     chrome.notifications.create('Song not found', {
       type: 'basic',
@@ -144,5 +148,119 @@ function songNotFound() {
     }, () => {
       console.log('song not found notification sent!')
     })
+  });
+};
+
+// *******************************************
+// **          AUTHENTICATION FLOW          **
+// *******************************************
+async function spotifyLogin() {
+  const response_type = "code";
+  const client_id = "cc9e2365a9c1461ea9a251d446f347d0";
+  const redirect_uri = chrome.identity.getRedirectURL();
+  const scope = "playlist-modify-private playlist-read-private user-library-modify";
+  const state = ranString();
+  const code_verifier = ranString();
+  const code_challenge = await challenge_from_verifier(code_verifier);
+  const code_challenge_method = "S256";
+
+  let url =
+    `https://accounts.spotify.com/authorize?response_type=${response_type}` +
+    `&client_id=${client_id}` +
+    `&redirect_uri=${redirect_uri}` +
+    `&scope=${scope}` +
+    `&code_challenge=${code_challenge}` +
+    `&code_challenge_method=${code_challenge_method}`;
+
+  chrome.identity.launchWebAuthFlow(
+    {
+      url: url,
+      interactive: true,
+    },
+    (redirectUrl) => {
+      const url = new URL(redirectUrl);
+      const params = new URLSearchParams(url.search);
+      let authToken = params.get("code");
+
+      if (authToken === null) {
+        console.log("failed to retrieve auth token");
+      } else {
+        const queryString =
+          `https://accounts.spotify.com/api/token` +
+          `?client_id=${client_id}` +
+          `&grant_type=authorization_code` +
+          `&code=${authToken}` +
+          `&redirect_uri=${redirect_uri}` +
+          `&code_verifier=${code_verifier}`;
+
+        fetch(queryString, {
+          method: "POST",
+          mode: "cors",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data)
+            chrome.storage.sync.set(
+              {
+                refreshToken: data.refresh_token,
+                accessToken: data.access_token,
+                timeStamp: Date.now(),
+              },
+              () => {
+                const payload = {
+                  message: 'logged in'
+                };
+
+                chrome.runtime.sendMessage(payload);
+              }
+            );
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    }
+  );
+};
+
+function ranString() {
+  str = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < 56; i++) {
+    str += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return str;
+};
+
+function sha256(plain) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(plain);
+  return crypto.subtle.digest("SHA-256", data);
+};
+
+function base64urlencode(hash) {
+  let str = "";
+  const bytes = new Uint8Array(hash);
+  const len = bytes.byteLength;
+  for (let i = 0; i < len; i++) {
+    str += String.fromCharCode(bytes[i]);
+  }
+  return btoa(str).replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
+};
+
+async function challenge_from_verifier(verifier) {
+  let hashedString = await sha256(verifier);
+  let base64encoded = base64urlencode(hashedString);
+  return base64encoded;
+};
+
+// Retrieve data stored in local storage. Find item by key
+async function getLocalValue(key) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(key, (data) => {
+      resolve(data);
+    });
   });
 };
--- frontend/js/popup.js
+++ frontend/js/popup.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", async () => {
+	chrome.runtime.onMessage.addListener((message, sender) => {
+		if (message.message == 'logged in') {
+			removeLoginButton();
+		};
+	});
+
 	document.getElementById("spotify-login").addEventListener("click", spotifyLogin);
 
 	// Hide any notification badges currently showing
@@ -33,106 +39,21 @@ document.addEventListener("DOMContentLoaded", async () => {
 		};
 	});
 
-	async function spotifyLogin() {
-		const response_type = "code";
-		const client_id = "cc9e2365a9c1461ea9a251d446f347d0";
-		const redirect_uri = chrome.identity.getRedirectURL();
-		const scope = "playlist-modify-private playlist-read-private user-library-modify";
-		const state = ranString();
-		const code_verifier = ranString();
-		const code_challenge = await challenge_from_verifier(code_verifier);
-		const code_challenge_method = "S256";
+	// Call authentication flow function in background.js
+	function spotifyLogin() {
+		const payload = {
+			message: 'spotify login',
+		};
 
-		let url =
-			`https://accounts.spotify.com/authorize?response_type=${response_type}` +
-			`&client_id=${client_id}` +
-			`&redirect_uri=${redirect_uri}` +
-			`&scope=${scope}` +
-			`&code_challenge=${code_challenge}` +
-			`&code_challenge_method=${code_challenge_method}`;
-
-		chrome.identity.launchWebAuthFlow(
-			{
-				url: url,
-				interactive: true,
-			},
-			(redirectUrl) => {
-				const url = new URL(redirectUrl);
-				const params = new URLSearchParams(url.search);
-				let authToken = params.get("code");
-
-				if (authToken === null) {
-					console.log("failed to retrieve auth token");
-				} else {
-					const queryString =
-						`https://accounts.spotify.com/api/token` +
-						`?client_id=${client_id}` +
-						`&grant_type=authorization_code` +
-						`&code=${authToken}` +
-						`&redirect_uri=${redirect_uri}` +
-						`&code_verifier=${code_verifier}`;
-
-					fetch(queryString, {
-						method: "POST",
-						mode: "cors",
-						headers: { "Content-Type": "application/x-www-form-urlencoded" },
-					})
-						.then((response) => response.json())
-						.then((data) => {
-							console.log(data)
-							chrome.storage.sync.set(
-								{
-									refreshToken: data.refresh_token,
-									accessToken: data.access_token,
-									timeStamp: Date.now(),
-								},
-								() => {
-									// Hide log in button and display logged in message on extension popup
-									document.getElementById('spotify-login').style.display = 'none';
-									let loggedInMessage = document.createElement('p');
-									loggedInMessage.innerHTML = 'You are logged in to Spotify!'
-									document.getElementById('login-flow').appendChild(loggedInMessage);
-								}
-							);
-						})
-						.catch((error) => {
-							console.log(error);
-						});
-				}
-			}
-		);
+		chrome.runtime.sendMessage(payload)
 	};
 
-	function ranString() {
-		str = "";
-		let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-		for (let i = 0; i < 56; i++) {
-			str += possible.charAt(Math.floor(Math.random() * possible.length));
-		}
-		return str;
-	};
-
-	function sha256(plain) {
-		const encoder = new TextEncoder();
-		const data = encoder.encode(plain);
-		return window.crypto.subtle.digest("SHA-256", data);
-	};
-
-	function base64urlencode(hash) {
-		let str = "";
-		const bytes = new Uint8Array(hash);
-		const len = bytes.byteLength;
-		for (let i = 0; i < len; i++) {
-			str += String.fromCharCode(bytes[i]);
-		}
-		return btoa(str).replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
-	};
-
-	async function challenge_from_verifier(verifier) {
-		let hashedString = await sha256(verifier);
-		let base64encoded = base64urlencode(hashedString);
-		return base64encoded;
+	// Remove login button after user has logged in
+	function removeLoginButton() {
+		document.getElementById('spotify-login').style.display = 'none';
+		let loggedInMessage = document.createElement('p');
+		loggedInMessage.innerHTML = 'You are logged in to Spotify!'
+		document.getElementById('login-flow').appendChild(loggedInMessage);
 	};
 
 	// Retrieve data stored in local storage. Find item by key
