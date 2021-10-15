90f880ba82c862905fcc99eb92f2c95be2461e0d
Jackie <taekwondo.chan@gmail.com>
2021-10-14 18:59:56 -0700

    convert webm with ffmpeg

--- backend/src/helpers.js
+++ backend/src/helpers.js
@@ -1,84 +1,97 @@
 require("dotenv").config();
 let fs = require("fs");
 let ytdl = require("ytdl-core");
+const ffmpeg = require("fluent-ffmpeg");
 const API_KEY = process.env.API_KEY;
 const axios = require("axios");
 const FormData = require("form-data");
 const util = require("util");
 
 const convertVideo = (url, accessToken) => {
+	let videoId = url.split("?v=")[1];
 	return new Promise((resolve, reject) => {
-		const songPath = "./audio/newvid.webm";
-		// let startTime = Date.now();
-		// let endTime;
-		// let chunkSize;
+		const webmPath = `./audio/${videoId}.webm`;
+		const audioPath = `./audio/${videoId}.mp3`;
+		let startTime = Date.now();
 
 		ytdl.getInfo(url).then((info) => {
 			let webm = ytdl.downloadFromInfo(info, {
 				filter: "audioonly",
 				quality: "lowest",
 			});
-			webm.pipe(fs.createWriteStream(songPath));
+			webm.pipe(fs.createWriteStream(webmPath));
 			console.log("Downloading...");
 
-			// webm.on("progress", (a, b, c) => {
-			// 	console.log(a, b, c);
-			// });
-
 			webm.on("end", () => {
-				// endTime = Date.now();
-				// let elapsedTime = endTime - startTime;
-				// console.log(`${elapsedTime / 1000} secs, chunk: ${chunkSize}`);
+				let endTime = Date.now();
+				let elapsedTime = endTime - startTime;
+				console.log(`${elapsedTime / 1000} secs`);
 
-				console.log("Sending...");
-				var data = new FormData();
-				data.append("file", fs.createReadStream(songPath));
-				data.append("api_token", API_KEY);
-				data.append("return", "spotify");
+				// Convert webm to mp3
+				ffmpeg(webmPath)
+					.duration(24)
+					.format("mp3")
+					.save(audioPath)
+					.on("error", (err) => console.log(`Error converting: ${err.message}`))
+					.on("start", () => console.log("Converting..."))
+					.on("end", () => {
+						console.log("Conversion complete. Sending audio.");
 
-				var config = {
-					method: "post",
-					url: "https://api.audd.io/",
-					headers: {
-						...data.getHeaders(),
-					},
-					data: data,
-				};
+						// Construct audd.io payload
+						var data = new FormData();
+						data.append("file", fs.createReadStream(audioPath));
+						data.append("api_token", API_KEY);
+						data.append("return", "spotify");
 
-				axios(config).then((res) => {
-					console.log(`Audd.io: ${res.data}`);
+						var config = {
+							method: "post",
+							url: "https://api.audd.io/",
+							headers: {
+								...data.getHeaders(),
+							},
+							data: data,
+						};
 
-					if (res.data.result != null) {
-						console.log(res.data.result);
-						// If song is recognized
+						// Send payload
+						axios(config).then((res) => {
+							// Delete webm and mp3
+							fs.unlinkSync(webmPath);
+							fs.unlinkSync(audioPath);
+							console.log(`Audd.io: ${res.data}`);
 
-						if (res.data.result.spotify) {
-							// If API returns spotify data
-							console.log("API returned spotify id");
-							resolve({
-								title: res.data.result.title,
-								artist: res.data.result.artist,
-								spotifyId: res.data.result.spotify.id,
-							});
-						} else {
-							// If API returns no spotify data
-							console.log("API did not return spotify id, searching spotify w/ song data");
-							searchSpotify(accessToken, res.data.result.title, res.data.result.artist)
-								.then((spotifyId) => {
+							if (res.data.result != null) {
+								// audd.io recognizes song
+								console.log(res.data.result);
+
+								if (res.data.result.spotify) {
+									// audd.io returns spotify data
+									console.log("API returned spotify id");
 									resolve({
 										title: res.data.result.title,
 										artist: res.data.result.artist,
-										spotifyId: spotifyId,
+										spotifyId: res.data.result.spotify.id,
 									});
-								})
-								.catch((err) => {
-									reject(err);
-								});
-						}
-					} else {
-						reject({ error: "No matching spotify song" });
-					}
-				});
+								} else {
+									// audd.io does not return spotify data
+									console.log("API did not return spotify id, searching spotify w/ song data");
+									searchSpotify(accessToken, res.data.result.title, res.data.result.artist)
+										.then((spotifyId) => {
+											resolve({
+												title: res.data.result.title,
+												artist: res.data.result.artist,
+												spotifyId: spotifyId,
+											});
+										})
+										.catch((err) => {
+											reject(err);
+										});
+								}
+							} else {
+								// audd.io does not recognize song
+								reject({ error: "No matching spotify song" });
+							}
+						});
+					});
 			});
 		});
 	});
